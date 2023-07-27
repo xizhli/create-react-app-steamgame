5eff007938bca8024ee85a86011198d66f64fd62
ttytan123 <122275090+ttytan123@users.noreply.github.com>
2023-07-27 21:40:32 +0800

    slight modification to Route 3 and added Route 8 for screenshots

--- server/routes.js
+++ server/routes.js
@@ -83,12 +83,12 @@ const newGames = async function (req, res) {
 
 // Route 3: GET /reviews_count/:a/:b
 const reviewsCount = async function (req, res) {
-  const a = req.params.a;
-  const b = req.params.b;
+  const lower_limit = req.params.a;
+  const upper_limit= req.params.b;
   const query3 = `
     SELECT G.id, COUNT(R.text)
     FROM Game G JOIN Review R ON G.id = R.GameID
-    WHERE G.price BETWEEN ${a} AND ${b}
+    WHERE G.price BETWEEN ${lower_limit} AND ${upper_limit}
     GROUP BY G.id;
   `;
 
@@ -191,6 +191,28 @@ const gameSelection = async function (req, res) {
     }
   });
 };
+
+//Route 8: GET /media_ss/:id
+const media_ss = async function(req, res){
+  
+  const game_id = req.params.id;
+
+  const query8 = `
+  SELECT Screenshots
+  FROM Media
+  WHERE GameID =${game_id}`;
+
+  connection.query(query8, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.json([]);
+    } else {
+      res.json(data);
+    }
+  });
+
+};
+
 
 module.exports = {
   popularGames,
