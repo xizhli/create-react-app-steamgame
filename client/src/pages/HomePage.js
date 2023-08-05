93b6b7118519a0d70e64bd4a1ab9cde9ca7345df
ttytan123 <122275090+ttytan123@users.noreply.github.com>
2023-08-05 21:42:02 +0800

    Fixed bug that didn't allow loading

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -3,14 +3,12 @@ import { Container, Divider, Link } from '@mui/material';
 import { NavLink } from 'react-router-dom';
 
 import LazyTable from '../components/LazyTable';
-//import SongCard from '../components/SongCard';
 const config = require('../config.json');
 
 export default function HomePage() {
   // We use the setState hook to persist information across renders (such as the result of our API calls)
   const [gameOfTheDay, setGameOfTheDay] = useState({});
-  // TODO (TASK 13): add a state variable to store the app author (default to '')
-  const [appAuthor, setAppAuthor] = useState('');
+
   const [selectedDev, setSelectedDev] = useState(null);
 
   // The useEffect hook by default runs the provided callback after every render
@@ -24,13 +22,7 @@ export default function HomePage() {
     // and proceeds to convert the result to a JSON which is finally placed in state.
     fetch(`http://${config.server_host}:${config.server_port}/random`)
       .then(res => res.json())
-      .then(resJson => setGameOfTheDay(resJson));
-
-    // TODO (TASK 14): add a fetch call to get the app author (name not pennkey) and store it in the state variable
-    // Hint: note that the app author is a string, not a JSON object. To convert to text, call res.text() instead of res.json()
-    fetch(`http://${config.server_host}:${config.server_port}/author/name`) // Task 14: add a fetch call to get the app author (name not pennkey) and store it in the state variable
-      .then(res => res.text()) // Convert the response to text
-      .then(author => setAppAuthor(author)); // Store the author in the state variable
+      .then(resJson => setGameOfTheDay(resJson[0]['name']));
   }, []);
 
   // Here, we define the columns of the "Top Songs" table. The songColumns variable is an array (in order)
@@ -48,16 +40,7 @@ export default function HomePage() {
       headerName: 'Count',
       
     }
-    /*
-    {
-      field: 'album',
-      headerName: 'Album Title',
-      renderCell: (row) => <NavLink to={`/albums/${row.album_id}`}>{row.album}</NavLink> // A NavLink component is used to create a link to the album page
-    },
-    {
-      field: 'plays',
-      headerName: 'Plays'
-    },*/
+
   ];
 
   // TODO (TASK 15): define the columns for the top albums (schema is Album Title, Plays), where Album Title is a link to the album page
@@ -73,33 +56,27 @@ export default function HomePage() {
       field: 'Count',
       headerName: 'Count',
     }
-    /*
-    {
-      field: 'plays',
-      headerName: 'Plays'
-    },*/
   ]
   
   return (
     <Container>
-     {/*selectedDev && <SongCard songId={selectedDev} handleClose={() => setSelectedDev(null)} />*/}
-     {selectedDev}
-      <h2>Check out your game of the day:&nbsp;{gameOfTheDay[0]['name']}
+      {/* SongCard is a custom component that we made. selectedSongId && <SongCard .../> makes use of short-circuit logic to only render the SongCard if a non-null song is selected */}
+      <h2>Check out your song of the day:&nbsp;
+      {String(gameOfTheDay)}
       </h2>
       <Divider />
-      <h2>Top Developers</h2> 
+      <h2>Top Songs</h2>
       <LazyTable route={`http://${config.server_host}:${config.server_port}/top_developers`} columns={devColumns} />
       <Divider />
-      <h2>Top Publishers</h2>
+      <h2>Top Albums</h2>
       {/* TODO (TASK 16): add a h2 heading, LazyTable, and divider for top albums. Set the LazyTable's props for defaultPageSize to 5 and rowsPerPageOptions to [5, 10] */}
       {/* TODO (TASK 17): add a paragraph (<p>text</p>) that displays the value of your author state variable from TASK 13 */}
-      
       <LazyTable
         route={`http://${config.server_host}:${config.server_port}/top_publishers`}
         columns={pubColumns}
-        //defaultPageSize={5}
-        //rowsPerPageOptions={[5, 10]}
-  /> 
+        defaultPageSize={5}
+        rowsPerPageOptions={[5, 10]}
+      />
       <Divider />
     </Container>
   );
